7a44448a5f83a54c390707e6636bc3baca461869
Franco <francoshen@126.com>
2025-12-23 23:21:13 +0800

    practise output parser for string, json, xml, yaml

---
+++ outputparser/xml_output_parser_stream.py
@@ -0,0 +1,26 @@
+from langchain_core.output_parsers import JsonOutputParser, XMLOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+from config.load_key import load_envkey
+
+model = ChatOpenAI(
+    model=load_envkey('MODEL_NAME'),
+    base_url=load_envkey('BASE_URL'),
+    api_key=load_envkey('API_KEY_DASHSCOPE')
+)
+
+joke_query = "生成周星驰的简化电影列表，按照最新时间的降序"
+# format result to defined model class
+# parser = XMLOutputParser()
+# enhanced tags
+parser = XMLOutputParser(tags=["movies", "actor", "film", "name", "genre"])
+prompt = PromptTemplate(
+    template="回答用户的查询，\n{format_instructions}\n{query}\n",
+    input_variables=["query"],
+    partial_variables={"format_instructions": parser.get_format_instructions()}
+)
+chain = prompt | model | parser
+response = chain.stream({"query": joke_query})
+for line in response:
+    print(line)
---
+++ outputparser/yaml_output_parser.py
@@ -0,0 +1,30 @@
+from langchain_classic.output_parsers import YamlOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+
+from config.load_key import load_envkey
+
+model = ChatOpenAI(
+    model=load_envkey('MODEL_NAME'),
+    base_url=load_envkey('BASE_URL'),
+    api_key=load_envkey('API_KEY_DASHSCOPE')
+)
+
+class Joke(BaseModel):
+    setup: str = Field(description="设置笑话的问题")
+    punchline: int = Field(description="解决笑话的答案")
+
+joke_query = "告诉我一个笑话，小于50字"
+# format result to defined model class
+parser = YamlOutputParser(pydantic_object=Joke)
+# no format result
+# parser = JsonOutputParser()
+prompt = PromptTemplate(
+    template="回答用户的查询，\n{format_instructions}\n{query}\n",
+    input_variables=["query"],
+    partial_variables={"format_instructions": parser.get_format_instructions()},
+)
+chain = prompt | model | parser
+response = chain.invoke({"query": joke_query})
+print(response)
